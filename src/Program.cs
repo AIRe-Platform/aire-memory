055591ef3919387eceb743c6ba3e41f27b7ed518
Niko Junnila <niko.junnila@goodlife.technology>
2023-12-28 16:09:20 +0200

    Remove runtime migrations (did not work when deployed)

--- src/Program.cs
+++ src/Program.cs
@@ -10,7 +10,6 @@ using Newtonsoft.Json;
 using Aire.Memory;
 using Aire.Sdk.Auth.Extensions;
 using Aire.Sdk.Auth.Models;
-using Microsoft.EntityFrameworkCore;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication(worker => {
@@ -47,11 +46,5 @@ var host = new HostBuilder()
         services.AddDbContext<DatabaseContext>();
     })
     .Build();
-
-using (var scope = host.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
-    db.Database.Migrate();
-}
 
 host.Run();
