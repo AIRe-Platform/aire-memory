2e9cb90d7222b009523c3cc0ddadde56f6c5374e
Niko Junnila <niko.junnila@goodlife.technology>
2024-01-17 12:17:51 +0200

    Fix OpenAPI configuration

--- src/Environment.cs
+++ src/Environment.cs
@@ -5,5 +5,6 @@ namespace Aire.Memory
         public static string? DatabaseConnectionString => Environment.GetEnvironmentVariable("DatabaseConnectionString");
         public static string? TokenSigningKey => Environment.GetEnvironmentVariable("TokenSigningKey");
         public static string? TokenEncryptionKey => Environment.GetEnvironmentVariable("TokenEncryptionKey");
+        public static string? OpenApiHost => Environment.GetEnvironmentVariable("OpenApi__HostNames");
     }
 }
