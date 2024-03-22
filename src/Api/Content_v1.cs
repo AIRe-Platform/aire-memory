c9e1507fd4d89daabd0f8ba6722cf4f87c2a5fc0
JuanDGoodlifeTechnology <juan.deltell@goodlife.technology>
2024-03-22 15:28:07 +0200

    Now it works GET, POST, PUT and DELETE. There is a bug when updating some value.

--- src/Api/Content_v1.cs
+++ src/Api/Content_v1.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Aire.Memory.Models;
@@ -13,23 +12,25 @@ using Aire.Sdk.AspNetCore;
 using Aire.Sdk.Auth;
 using Aire.Sdk.Models.Resources;
 using Aire.Sdk.Platform.Clients;
+using Aire.Sdk.Azure;
+using Aire.Sdk.Helpers;
 
 namespace Aire.Memory.Api;
 
 public class Content_v1
 {
-    private readonly DatabaseContext _db;
+    private readonly ITableStorageService _storage;
     private readonly IJwtTokenService _jwt;
     private readonly IAireClientFactory _clientFactory;
     private readonly ILogger _log;
 
     public Content_v1(
-        DatabaseContext db,
+        ITableStorageService storage,
         IJwtTokenService jwt,
         IAireClientFactory clientFactory,
         ILogger<Content_v1> log)
     {
-        _db = db;
+        _storage = storage;
         _jwt = jwt;
         _clientFactory = clientFactory;
         _log = log;
@@ -53,29 +54,17 @@ public class Content_v1
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "v1/contents")] HttpRequest req,
         FunctionContext context)
     {
-        /* var auth = context.Features.Get<JwtAuthFeature>();
+        var auth = context.Features.Get<JwtAuthFeature>();
         if (auth == null)
             return new UnauthorizedResult();
 
-        if (!_jwt.CheckAuthorization(auth, requiredScopes: AireScopes.ReadQuestionnaire))
+        if (!_jwt.CheckAuthorization(auth, requiredScopes: AireScopes.ReadContent))
              return new ForbiddenResult();
-        */
-        var contents = new List<Content>();
-        contents.Add( new Content{
-            Name = "Juan",
-            Description = "Description Juan",
-            Hidden = false,
-            Type = "image",
-            URL = "/src/assets/images/meme.jpg",
-            Age = "5-15",
-            Gender = "all",
-            Injured_type = "head",
-            Views_count = 21,
-            Viewers_rating = 4,
-        });
+       
+        var all = await _storage.All<ContentEntity>();
+        var list = all.Select(x => x.ToModel()).ToList();
         
-
-        return new ObjectResult(contents);
+        return new ObjectResult(list);
     }
 
     
@@ -100,92 +89,125 @@ public class Content_v1
         [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "v1/content")] HttpRequest req,
         FunctionContext context)
     {
-       /*  var auth = context.Features.Get<JwtAuthFeature>();
+        var auth = context.Features.Get<JwtAuthFeature>();
         if (auth == null)
             return new UnauthorizedResult();
- */
-        /* if (!_jwt.CheckAuthorization(auth, requiredScopes: AireScopes.WriteQuestionnaire))
-            return new ForbiddenResult(); */
+        
+        if (!_jwt.CheckAuthorization(auth, requiredScopes: AireScopes.WriteContent))
+            return new ForbiddenResult();
 
         var content = await req.ReadJson<Content>();
         if (content == null)
             return new BadRequestResult();
 
-       
+        content.Id = Guid.NewGuid();
+        var entity = new ContentEntity(content);
+
+        var add = await _storage.UpsertAsync(entity);
+        if (!add)
+            return new InternalServerErrorResult();
+
         return new ObjectResult(content);
     }
 
 
-    [Function("PutQuestionnaire_v1")]
+    [Function("PutContent_v1")]
     [OpenApiOperation(
-            operationId: "putQuestionnaire",
-            tags: ["questionnaire"],
-            Summary = "Edit existing questionnaire")]
+            operationId: "putContent",
+            tags: ["Content"],
+            Summary = "Edit existing Content")]
     [OpenApiSecurity(
             schemeName: "bearer_auth",
             schemeType: SecuritySchemeType.Http,
             Scheme = OpenApiSecuritySchemeType.Bearer,
             BearerFormat = "JWT",
             Description = "User token")]
-    [OpenApiParameter("id", Description = "Questionnaire identifier", Required = true)]
-    [OpenApiRequestBody("application/json", typeof(Questionnaire), Description = "Questionnaire", Required = true)]
-    [OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(Questionnaire), Description = "Questionnaire")]
-    [OpenApiResponseWithoutBody(HttpStatusCode.NotFound, Description = "The questionnaire was not found")]
+    [OpenApiParameter("id", Description = "Content identifier", Required = true)]
+    [OpenApiRequestBody("application/json", typeof(Content), Description = "Content", Required = true)]
+    [OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(Content), Description = "Content")]
+    [OpenApiResponseWithoutBody(HttpStatusCode.NotFound, Description = "The Content was not found")]
     [OpenApiResponseWithoutBody(HttpStatusCode.BadRequest, Description = "Invalid body or param")]
     [OpenApiResponseWithoutBody(HttpStatusCode.Unauthorized, Description = "Missing or insufficient authorization")]
-    public async Task<IActionResult> PutQuestionnaire(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "v1/questionnaire/{id}")] HttpRequest req,
+    public async Task<IActionResult> PutContent(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "v1/content/{id}")] HttpRequest req,
             FunctionContext context,
             string id)
     {
         var auth = context.Features.Get<JwtAuthFeature>();
-        if (!_jwt.CheckAuthorization(auth, requiredScopes: AireScopes.WriteQuestionnaire))
+        if (!_jwt.CheckAuthorization(auth, requiredScopes: AireScopes.WriteContent))
             return new UnauthorizedResult();
 
-        if (!Guid.TryParse(id, out Guid questionnaireId))
+        if (!Guid.TryParse(id, out Guid contentId))
             return new BadRequestResult();
 
-        var questionnaire = await req.ReadJson<Questionnaire>();
-        if (questionnaire == null)
+        var content = await req.ReadJson<Content>();
+        if (content == null)
             return new BadRequestResult();
 
-        var entity = await _db.Questionnaires
-            .Where(x => x.Id == questionnaireId)
-            .FirstOrDefaultAsync();
-
+        var entity = await _storage.RetrieveAsync<ContentEntity>(id);
         if (entity == null)
             return new NotFoundResult();
 
-        var questionnaireEntity = new QuestionnaireEntity(questionnaire);
-        // Updating existing tracked entity in database with new entity with same id will cause error, so clear tracker.
-        _db.ChangeTracker.Clear();
+        // Update entity data
+        if (content.Name != null)
+            entity.Name = content.Name;
 
-        var update = _db.Questionnaires.Update(questionnaireEntity);
-        await update.Context.SaveChangesAsync();
+        if (content.Description != null)
+            entity.Description =  content.Description;
 
-        return new ObjectResult(questionnaire);
+        if (content.Hidden != null)
+            entity.Hidden = content.Hidden;
+
+        if (content.Type != null)
+            entity.Type = content.Type;
+
+        if (content.Url != null)
+            entity.Url = content.Url;
+
+        if (content.ViewsCount != null)
+            entity.ViewsCount = content.ViewsCount;
+
+        if (content.ViewersRating != null)
+            entity.ViewersRating = content.ViewersRating;
+
+        if (content.InjuredType != null)
+            entity.InjuredType = content.InjuredType;
+
+        if (content.Age != null)
+            entity.Age = content.Age;
+
+        if (content.Gender != null)
+            entity.Gender = content.Gender;
+
+        // Apply edits
+
+        var save = await _storage.UpsertAsync(entity);
+        if (!save)
+            return new InternalServerErrorResult();
+
+        return new ObjectResult(content);
     }
 
 
-    [Function("DeleteQuestionnaire_v1")]
+    [Function("DeleteContent_v1")]
     [OpenApiOperation(
-        operationId: "deleteQuestionnaireWithId",
-        tags: ["questionnaire"],
-        Summary = "Delete a questionnaire")]
+        operationId: "deleteContentWithId",
+        tags: ["content"],
+        Summary = "Delete a content")]
     [OpenApiSecurity(
         schemeName: "bearer_auth",
         schemeType: SecuritySchemeType.Http,
         Scheme = OpenApiSecuritySchemeType.Bearer,
         BearerFormat = "JWT",
         Description = "User token")]
-    [OpenApiParameter("id", Description = "Questionnaire identifier", In = ParameterLocation.Path, Required = true)]
+    [OpenApiParameter("id", Description = "Content identifier", In = ParameterLocation.Path, Required = true)]
     [OpenApiResponseWithoutBody(HttpStatusCode.NoContent, Description = "Operation was successful")]
-    [OpenApiResponseWithoutBody(HttpStatusCode.NotFound, Description = "The questionnaire was not found.")]
+    [OpenApiResponseWithoutBody(HttpStatusCode.NotFound, Description = "The content was not found.")]
     [OpenApiResponseWithoutBody(HttpStatusCode.BadRequest, Description = "Invalid parameter")]
     [OpenApiResponseWithoutBody(HttpStatusCode.Unauthorized, Description = "Missing or insufficient authorization")]
     [OpenApiResponseWithoutBody(HttpStatusCode.Forbidden, Description = "Access denied")]
-    public async Task<IActionResult> DeleteQuestionnaire(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "v1/questionnaire/{id}")] HttpRequest req,
+    public async Task<IActionResult> DeleteContent(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "v1/content/{id}")] HttpRequest req,
         FunctionContext context,
         string id)
     {
@@ -193,39 +215,20 @@ public class Content_v1
         if (auth == null)
             return new UnauthorizedResult();
 
-        if (!_jwt.CheckAuthorization(auth, requiredScopes: AireScopes.DeleteQuestionnaire))
+        if (!_jwt.CheckAuthorization(auth, requiredScopes: AireScopes.DeleteContent))
             return new ForbiddenResult();
 
-        if (!Guid.TryParse(id, out Guid questionnaireId))
+        if (!Guid.TryParse(id, out Guid contentId))
             return new BadRequestResult();
 
-        var ent = await _db.Questionnaires
-            .Where(x => x.Id == questionnaireId)
-            .FirstOrDefaultAsync();
-
-        if (ent == null)
+        var entity = await _storage.RetrieveAsync<ContentEntity>(id);
+        if (entity == null)
             return new NotFoundResult();
-
-        if (ent.EmbeddingId != null)
-        {
-            var aiService = await _clientFactory.CreateAiClient(auth!.JwtEncodedToken);
-            if (aiService == null)
-            {
-                _log.LogCritical("Default AI module not configured");
-                return new InternalServerErrorResult();
-            }
-
-            bool result = await aiService.DeleteQuestionnaireEmbedding(ent.EmbeddingId.ToString()!);
-            if (!result)
-            {
-                _log.LogCritical("Failed to delete questionnaire embeddings");
-                return new InternalServerErrorResult();
-            }
-        }
-
-        _db.Questionnaires.Remove(ent);
-        await _db.SaveChangesAsync();
-
+    
+        var delete = await _storage.DeleteAsync(entity);
+        if (!delete)
+            return new InternalServerErrorResult();
+        
         return new NoContentResult();
     }
 }
--- src/Models/ContentEntity.cs
+++ src/Models/ContentEntity.cs
@@ -1,26 +1,71 @@
+using Aire.Sdk.Azure;
 using Aire.Sdk.Helpers;
 using Aire.Sdk.Models.Resources;
 
-namespace Aire.Memory.Models
+namespace Aire.Memory.Models;
+
+/// <summary>
+/// ID: PartitionKey, Rowkey
+/// </summary>
+[EntityTable("Contents")]
+public class ContentEntity : BaseTableEntity
 {
-    public class ContentEntity
+    public Guid? Id { get; set; } = Guid.NewGuid();
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public bool? Hidden { get; set; }
+    public string? Type { get; set; }
+    public string? Url { get; set; }
+    public int? ViewsCount { get; set; }
+    public int? ViewersRating { get; set; }
+    public string? InjuredType { get; set; }
+    public string? Age { get; set; }
+    public string? Gender { get; set; }
+
+    public ContentEntity() 
+    { 
+        string id = Guid.NewGuid().ToString();
+        PartitionKey ??= id;
+        RowKey ??= id;
+    }
+
+    public ContentEntity(Content content) 
+    { 
+        PartitionKey = content.Id.ToString();
+        RowKey = content.Id.ToString();
+        Name = content.Name;
+        Description = content.Description;
+        Hidden = content.Hidden;
+        Type = content.Type;
+        Url = content.Url; 
+        ViewsCount = content.ViewsCount;
+        ViewersRating = content.ViewersRating;
+        InjuredType = content.InjuredType;
+        Age = content.Age;
+        Gender = content.Gender;
+    }
+
+    public Content ToModel()
     {
-        public Guid? Id { get; set; } = Guid.NewGuid();
-        public string? Name { get; set; }
-        public string? Description { get; set; }
-        public bool? Hidden { get; set; }
-        public string? Type { get; set; }
-        public string? URL { get; set; }
-        public int? views_count { get; set; }
-        public int? viewers_rating { get; set; }
-        public string? Injured_type { get; set; }
-        public string? Age { get; set; }
-        public string? Gender { get; set; }
-        public DateTime Modified { get; set; }
+        var model = new Content
+        {
+            Name = Name,
+            Description = Description,
+            Modified = Timestamp.HasValue ? Timestamp.Value.UtcDateTime : DateTime.UtcNow,
+            Hidden = Hidden,
+            Type = Type,
+            Url = Url, 
+            ViewsCount = ViewsCount,
+            ViewersRating = ViewersRating,
+            InjuredType = InjuredType,
+            Age = Age,
+            Gender = Gender
+        };
 
+        if (Guid.TryParse(RowKey, out var id))
+            model.Id = id;
 
-        public ContentEntity() { }
-
-        
+        return model;
     }
 }
+
